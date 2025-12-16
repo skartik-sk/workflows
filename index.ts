87cafecd55e510c8decdd42be7e1f82936c476a0
Singupalli Kartik <skartik1706@gmail.com>
2025-12-16 23:42:49 +0530

    make workder

--- index.ts
+++ index.ts
@@ -7,13 +7,15 @@ import {
     type Address,
 } from '@solana/kit';
 
+import express from 'express';
+
 console.log('🚀 Airdrop scheduler started!');
 console.log('⏰ Cron schedule: Every 10 minutes (keeps Render awake)');
 console.log('⏱️  Current time:', new Date().toISOString());
 console.log('📍 Runs at: :00, :10, :20, :30, :40, :50 of each hour');
 console.log('---');
 
-nodeCron.schedule("*/10 * * * *", async () => {
+nodeCron.schedule("0 */5 * * *", async () => {
     console.log('\n🔄 Starting airdrop at', new Date().toISOString());
     try {
         const rpc = createSolanaRpc("https://api.devnet.solana.com");
@@ -31,5 +33,25 @@ nodeCron.schedule("*/10 * * * *", async () => {
         console.error('❌ Airdrop failed:', error);
     }
 });
+nodeCron.schedule("*/10 * * * *", async () => {
+    console.log('\n🔄 Starting airdrop at', new Date().toISOString());
+    try {
+        const res= await fetch('https://workflows-urgl.onrender.com', {
+            method: 'GET',});
+        console.log('✅ Wakeup ping sent successfully!', res.status);
+    } catch (error) {
+        console.error('❌ Airdrop failed:', error);
+    }
+});
 
 
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Airdrop scheduler is running!');
+});
+
+app.listen(PORT, () => {
+    console.log(`🌐 Server is listening on port ${PORT}`);
+});
